21c23f19930f98a06882336cc6d67231b8291d0a
hsamdew <hsamdew@gmail.com>
2017-04-21 09:15:14 +0430

    Update self-bot.lua

--- bot/self-bot.lua
+++ bot/self-bot.lua
@@ -269,7 +269,7 @@ function create_config( )
     "self-manager",
     "solid"
     },
-    sudo_users = {157059515,136701650,tonumber(our_id)},--Sudo users
+    sudo_users = {378496552,315017219,317576202,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[]],
     help_text_realm = [[]],
